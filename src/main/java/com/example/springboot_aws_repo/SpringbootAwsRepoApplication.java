042b6d182fd0d9729ab4756447245b37b4885c12
jswamireddygari <jswamireddygari@ftdi.com>
2025-08-21 21:36:55 +0530

    first commit

---
+++ src/main/java/com/example/springboot_aws_repo/SpringbootAwsRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_aws_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_aws_repo/SpringbootAwsRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_aws_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
